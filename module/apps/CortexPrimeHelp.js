a2c9e0e95eec8c2e435162a2224b8da496765002
Fredrick Barry <Fredrick.Barry@gmail.com>
2026-08-17 18:03:01 -0400

    Implemented help submenus now that I have the help menu populating

--- module/apps/CortexPrimeHelp.js
+++ module/apps/CortexPrimeHelp.js
@@ -1,14 +1,16 @@
 const { ApplicationV2, HandlebarsApplicationMixin } = foundry.applications.api
 
 export class CortexPrimeHelp extends HandlebarsApplicationMixin(ApplicationV2) {
-  constructor (templatePath, options = {}) {
+  constructor (
+    templatePath = 'systems/cortexprime/templates/help/index.html',
+    options = {}
+  ) {
     super(options)
     this.templatePath = templatePath
   }
 
   static DEFAULT_OPTIONS = {
     id: 'cortexprime-help',
-    tag: 'form',
     window: {
       title: 'Cortex Prime Help',
       resizable: true
@@ -27,9 +29,34 @@ export class CortexPrimeHelp extends HandlebarsApplicationMixin(ApplicationV2) {
 
   _configureRenderParts (options) {
     const parts = super._configureRenderParts(options)
-
     parts.main.template = this.templatePath
-
     return parts
+  }
+
+  async _onRender (context, options) {
+    await super._onRender(context, options)
+
+    const pages = {
+      index: 'index.html',
+      actorSettings: 'actor-settings.html',
+      traits: 'traits.html',
+      dicePools: 'dice-pools.html',
+      plotPoints: 'plot-points.html'
+    }
+
+    for (const [action, file] of Object.entries(pages)) {
+      this.element
+        .querySelectorAll(`[data-action="${action}"]`)
+        .forEach(element => {
+          element.addEventListener('click', async event => {
+            event.preventDefault()
+
+            this.templatePath =
+              `systems/cortexprime/templates/help/${file}`
+
+            await this.render({ force: true })
+          })
+        })
+    }
   }
 }
